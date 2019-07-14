a3c4b3dd26669c811109291ad021eb19f4e6e9e3
Crom <james.vogon@gmail.com>
2019-07-13 22:27:56 -0700

    Initial version - with worgs

---
+++ LegendaryDogsWorgHero1.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XRL.Core;
+using XRL.Names;
+using XRL.Rules;
+using XRL.World.Encounters;
+using XRL.World.Parts.Mutation;
+
+namespace XRL.World.Parts
+{
+	[Serializable]
+	public class LegendaryDogsWorgHero1 : IPart
+	{
+		public bool bCreated;
+
+		public static string[] Prefixes = new string[16]
+		{
+			"be",
+			"ba",
+			"ta",
+			"to",
+			"ru",
+			"ro",
+			"te",
+			"ta",
+			"da",
+			"du",
+			"di",
+			"do",
+			"u",
+			"a",
+			"i",
+			"pu"
+		};
+
+		public static string[] Infixes = new string[10]
+		{
+			"d",
+			"dd",
+			"m",
+			"mm",
+			"nn",
+			"n",
+			"p",
+			"pp",
+			"g",
+			"gg"
+		};
+
+		public static string[] Postfixes = new string[5]
+		{
+			"y",
+			"o",
+			"a",
+			"i",
+			"us"
+		};
+
+		public LegendaryDogsWorgHero1()
+		{
+			base.Name = "LegendaryDogsWorgHero1";
+		}
+
+		public override bool SameAs(IPart p)
+		{
+			return false;
+		}
+
+		public override void Register(GameObject Object)
+		{
+			Object.RegisterPartEvent(this, "EnteredCell");
+		}
+
+		public override bool FireEvent(Event E)
+		{
+			if (E.ID == "EnteredCell")
+			{
+				try
+				{
+					if (bCreated)
+					{
+						return true;
+					}
+					bCreated = true;
+					GameObject parentObject = ParentObject;
+
+					StringBuilder NameBuilder = new StringBuilder();
+					NameBuilder.Append("&M");
+					string Prefix = Prefixes.GetRandomElement();
+					NameBuilder.Append(char.ToUpper(Prefix[0]));
+					NameBuilder.Append(Prefix.Substring(1));
+
+					if (Stat.RandomCosmetic(1,2) == 2) {
+						NameBuilder.Append(Prefixes.GetRandomElement());
+					}
+					NameBuilder.Append(Infixes.GetRandomElement());
+					NameBuilder.Append(Postfixes.GetRandomElement());
+					NameBuilder.Append(", Awe-inspiring ");
+					NameBuilder.Append(parentObject.DisplayName);
+
+					parentObject.DisplayName = NameBuilder.ToString();
+				}
+				catch
+				{
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ LegendaryDogsWorgPack1.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using XRL.Core;
+using XRL.Rules;
+using XRL.World.Encounters.EncounterObjectBuilders;
+
+namespace XRL.World.Parts
+{
+	[Serializable]
+	public class LegendaryDogsWorgPack1 : IPart
+	{
+		//
+		// Fields
+		//
+		public bool bCreated;
+
+		//
+		// Constructors
+		//
+		public LegendaryDogsWorgPack1 ()
+		{
+			base.Name = "LegendaryDogsWorgPack1";
+		}
+
+		//
+		// Methods
+		//
+		public override bool FireEvent (Event E)
+		{
+			if (E.ID == "EnteredCell") {
+				try {
+					if (this.bCreated) {
+						return true;
+					}
+					this.bCreated = true;
+
+					Physics physics = this.ParentObject.GetPart ("Physics") as Physics;
+					List<Cell> cellList = new List<Cell> ();
+					physics.CurrentCell.GetAdjacentCells (4, cellList, true);
+					List<Cell> emptyCellList = new List<Cell> ();
+					foreach (Cell current in cellList) {
+						if (current.IsEmpty ()) {
+							emptyCellList.Add (current);
+						}
+					}
+					List<string> randomWorgs = new List<string> ();
+					int worgs = Stat.Random (2, 5);
+					for (int i = 0; i < worgs; i++) {
+						randomWorgs.Add ("LegendaryDogs_Worg");
+					}
+					Tier1HumanoidEquipment tier1HumanoidEquipment = new Tier1HumanoidEquipment ();
+					int worgIterator = 0;
+					while (worgIterator < randomWorgs.Count && emptyCellList.Count > 0) {
+						GameObject gameObject = GameObjectFactory.Factory.CreateObject (randomWorgs [worgIterator]);
+						tier1HumanoidEquipment.BuildObject (gameObject, null);
+						gameObject.GetPart<Brain> ().PartyLeader = this.ParentObject;
+						Cell randomElement = emptyCellList.GetRandomElement (null);
+						randomElement.AddObject (gameObject);
+						XRLCore.Core.Game.ActionManager.AddActiveObject (gameObject);
+						emptyCellList.Remove (randomElement);
+						worgIterator++;
+					}
+				}
+				catch {
+				}
+				return true;
+			}
+			return true;
+		}
+
+		public override void Register (GameObject Object)
+		{
+			Object.RegisterPartEvent (this, "EnteredCell");
+		}
+
+		public override bool SameAs (IPart p)
+		{
+			return false;
+		}
+	}
+}
